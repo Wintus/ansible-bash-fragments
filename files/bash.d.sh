baa5007e6d85109ee2fbebe874fafed614c02ccd
wintus <yuya0726+GitHub@gmail.com>
2020-04-03 21:37:43 +0900

    Fix path of quote & substitution

--- files/bash.d.sh
+++ files/bash.d.sh
@@ -2,11 +2,11 @@
 
 function load_bash_fragments() {
     local frag
-    for frag in ${1%/}/*.sh; do
+    for frag in ${1}/*.sh; do
         if [[ -x "${frag}" ]]; then
             source "${frag}"
         fi
     done
 }
 
-load_bash_fragments $HOME/.bash.d/
+load_bash_fragments "$HOME/.bash.d/"
